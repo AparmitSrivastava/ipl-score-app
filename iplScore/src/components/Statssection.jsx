b92fe572c3eb89fb6ab956537fd1fedc64584900
AparmitSrivastava <aparmitsrivastava1@gmail.com>
2025-04-17 17:33:06 +0530

    another component added

---
+++ iplScore/src/components/Statssection.jsx
@@ -0,0 +1,17 @@
+"use client"
+import {motion} from "framer-motion"
+import  {Users,Trophy,Calendar,TrendingUp, Calendar} from "lucide-react"
+
+
+export default function Statssection(){
+    const stats = [
+        {icon:Users , title:"Total Matches" , value:"74"},
+        {icon:Trophy , title:"Team" , value:"74"},
+        {icon:Calendar , title:"Tournament Duration" , value:"74"},
+        {icon:TrendingUp , title:"Highest" , value:"74"},
+    ]
+    return (
+        <div></div>
+    )
+
+}
